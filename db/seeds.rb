b4be5c08d5eddcef49790a4c5b19532657732a5c
Vev <vev.gaunt@gmail.com>
2019-08-20 19:40:33 +0100

    seeded users and messages, displayed messages, made message partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create(username: 'Vev', password_digest: 'password')
+# User.create(username: 'Morty', password_digest: 'password')
+
+# Message.create(body: 'Hey bish', user_id: 1)
+# Message.create(body: 'Hey bitch', user_id: 2)
+# Message.create(body: 'Stop talking to yourself you\'re weird', user_id: 2)
+# Message.create(body: 'Fair point', user_id: 2)
